a12e28b7eae5f04660038eb52a7f894b53ded4d1
dasiksaol <2023218ol@aupp.edu.kh>
2023-08-02 00:52:25 +0700

    midnight brain teaser

--- binary_con.py
+++ binary_con.py
@@ -9,4 +9,23 @@ def binary_con(opt, value):
 
     example binary_con('bd', 1001) should return 9 as int value, and binary_con('db', 8) should return 1000 as int.
     '''
-    ...
+    if opt == "db":
+        result = ""
+        while value >= 2:
+            if value >= 2:
+                remainder = value % 2 
+                result += str(remainder)
+                value = value // 2
+                if value < 2:
+                    result += str(value)
+        return int(result[::-1])
+    elif opt == "bd":
+        result = 0
+        value = str(value)[::-1]
+        for index, x in enumerate(value):
+            result += ( (2**index) * int(x) )
+        return result
+
+
+# print(binary_con("bd", 10010001))
+# print(binary_con("db", 145))
